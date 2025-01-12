02eb2a953492a2f18d9815ea3eaae42432c44101
Gnanaprakash J <gnanaprakash.j@games24x7.com>
2025-01-12 21:43:23 +0530

    communicating with user service

--- src/main/java/dev/danvega/runnerz/Application.java
+++ src/main/java/dev/danvega/runnerz/Application.java
@@ -1,9 +1,19 @@
 package dev.danvega.runnerz;
 
+import dev.danvega.runnerz.user.User;
+import dev.danvega.runnerz.user.UserHttpClient;
+import dev.danvega.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -21,5 +31,22 @@ public class Application {
 //			runRepository.create(run);
 //		};
 //	}
+
+		@Bean
+		UserHttpClient userHttpClient() {
+			RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
+			HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+			return factory.createClient(UserHttpClient.class);
+		}
+
+		@Bean
+		CommandLineRunner runner(UserHttpClient client) {
+		return args -> {
+		List<User> users = client.findAll();
+			System.out.println(users);
+			User user=client.findById(1);
+			System.out.println(user);
+		};
+	}
 
 }
